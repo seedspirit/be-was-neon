02cc642a3162f2233c83a6ca245b5890ca3ae872
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-14 14:17:36 +0900

    chore: 불필요한 import 제거
    - 사용하지 않는 import 제거

--- src/main/java/webserver/HttpRequestMsg.java
+++ src/main/java/webserver/HttpRequestMsg.java
@@ -3,11 +3,6 @@ package webserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 public class HttpRequestMsg {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private String method;
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,7 +2,6 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
