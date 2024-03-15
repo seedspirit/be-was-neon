2625ac9726871aab7a9f124492d2a14b8c35c591
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-15 10:51:58 +0900

    fix: 파일 읽어오는 로직 수정
    - 기존 줄 단위 읽기 방식은 빈줄을 만나면 읽기를 중단
    - 미리 File의 길이를 알아내고 그만큼을 한 번에 읽는 방식으로 변경

--- src/main/java/webserver/ResourceLoader.java
+++ src/main/java/webserver/ResourceLoader.java
@@ -2,16 +2,17 @@ package webserver;
 
 import webserver.exceptions.ResourceNotFoundException;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class ResourceLoader {
     public String load(String path) throws ResourceNotFoundException {
-        try {
-            FileReader fileReader = new FileReader(path);
-            BufferedReader br = new BufferedReader(fileReader);
-            return util.Reader.bufferedReaderToString(br);
+        File targetFile = new File(path);
+        try (FileReader fileReader = new FileReader(targetFile)){
+            char[] chars = new char[(int) targetFile.length()];
+            fileReader.read(chars);
+            return new String(chars);
         } catch (IOException e) {
             throw new ResourceNotFoundException("요청한 %s 리소스가 존재하지 않습니다", path);
         }
