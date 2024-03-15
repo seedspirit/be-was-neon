576c65ffa8fda0ee466d37e533aaa2f55efa6048
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-15 11:02:26 +0900

    feat: 다양한 콘텐츠 타입 지원을 위한 enum 추가
    - 확장자와 mime타입 한 번에 관리할 수 있도록 구조 설정

---
+++ src/main/java/webserver/ContentType.java
@@ -0,0 +1,37 @@
+package webserver;
+
+import java.util.Arrays;
+
+public enum ContentType {
+    HTML(".html", "text/html"),
+    CSS(".css", "text/css"),
+    JS(".js", "text/javascript"),
+    ICO(".ico", "image/x-icon"),
+    PNG(".png", "image/png"),
+    JPG(".jpg", "image/jpeg"),
+    SVG(".svg", "image/svg+xml"),
+    NONE("", "");
+
+    private final String extension;
+    private final String mimeType;
+
+    ContentType(String extension, String mimeType) {
+        this.extension = extension;
+        this.mimeType = mimeType;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public String getMimetype() {
+        return mimeType;
+    }
+
+    public static ContentType findMatchingContentType(String requestLine){
+        return Arrays.stream(values())
+                .filter(v -> requestLine.contains(v.getExtension()))
+                .findFirst()
+                .orElse(NONE);
+    }
+}
