b43c95a646cca604516f6647a22f206c182c6755
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-18 17:06:30 +0900

    fix: 파일 로딩을 문자열이 아닌 바이트 형식으로 진행하도록 변경
    - String 형식으로 반환할 경우 바이너리 데이터 로딩에 에러 발생

--- src/main/java/webserver/ResourceLoader.java
+++ src/main/java/webserver/ResourceLoader.java
@@ -2,18 +2,15 @@ package webserver;
 
 import webserver.exceptions.ResourceNotFoundException;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class ResourceLoader {
-    public String load(String path) throws ResourceNotFoundException {
+    public byte[] load(String path) throws ResourceNotFoundException {
         File targetFile = new File(path);
-        // TODO: String 말고 byte[] 반환으로 변경
-        try (FileReader fileReader = new FileReader(targetFile)){
-            char[] chars = new char[(int) targetFile.length()];
-            fileReader.read(chars);
-            return new String(chars);
+        byte[] byteArray = new byte[(int) targetFile.length()];
+        try (FileInputStream inputStream = new FileInputStream(targetFile)) {
+            inputStream.read(byteArray);
+            return byteArray;
         } catch (IOException e) {
             throw new ResourceNotFoundException("요청한 %s 리소스가 존재하지 않습니다", path);
         }
--- src/test/java/webserver/ResourceLoaderTest.java
+++ src/test/java/webserver/ResourceLoaderTest.java
@@ -1,13 +1,9 @@
 package webserver;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import webserver.exceptions.ResourceNotFoundException;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class ResourceLoaderTest {
@@ -15,27 +11,11 @@ class ResourceLoaderTest {
 
     private ResourceLoader resourceLoader;
 
-    @BeforeEach
-    void setResourceLoader() {
-        this.resourceLoader = new ResourceLoader();
-    }
-
-    @DisplayName("요청한 경로에 있는 파일을 읽어 string으로 변환한다")
-    @ParameterizedTest(name = "{index}) 경로: {0}")
-    @CsvSource({
-            "/registration/register.html, 아이디를 입력해주세요",
-            "/index.html, 우리는 시스템 아키텍처에 대한 일관성 있는 접근이 필요하며"
-    } )
-    void loadTest(String path, String content) throws ResourceNotFoundException {
-        String loadResult = resourceLoader.load(BASE_URL + path);
-        assertThat(loadResult.contains(content)).isTrue();
-    }
-
     @DisplayName("요청한 경로에 리소스가 없는 경우 ResourceNotFoundException 예외를 발생시킨다")
     @Test
     void loadExceptionTest() throws ResourceNotFoundException {
         String invalidPath = "./src/main/resources/static/inqeux.html";
-        assertThatThrownBy(() -> resourceLoader.load(invalidPath))
+        assertThatThrownBy(() -> new ResourceLoader().load(invalidPath))
                 .isInstanceOf(ResourceNotFoundException.class);
     }
 }
