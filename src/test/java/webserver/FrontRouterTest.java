9cf50e16f83012be23fbca1b48bc3f050c4d2268
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-20 22:03:38 +0900

    test: 지원하지 않은 메서드 입력에 대한 테스트 코드 추가

--- src/test/java/webserver/FrontRouterTest.java
+++ src/test/java/webserver/FrontRouterTest.java
@@ -2,6 +2,7 @@ package webserver;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import util.Reader;
@@ -12,8 +13,11 @@ import webserver.router.FrontRouter;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
+import static webserver.httpMessage.HttpStatus.METHOD_NOT_ALLOWED;
 
 class FrontRouterTest {
 
@@ -43,14 +47,44 @@ class FrontRouterTest {
                 """;
 
         String requestExample = String.format(requestTemplate, requestPath);
-        InputStream is = new ByteArrayInputStream(requestExample.getBytes());
-        BufferedReader br = Reader.inputStreamToBufferedReader(is);
-        HttpRequest httpRequest = new HttpRequest(br);
 
-        HttpResponse actualResponseMsg = frontRouter.route(httpRequest);
-        HttpResponse expectedResponseMsg = new HttpResponse.Builder(expectedStatusCode, expectedReasonPhrase).build();
+        HttpResponse actualResponse = sendRequestAndGetResponse(requestExample);
+        HttpResponse expectedResponse = generateExpectedResponse(expectedStatusCode, expectedReasonPhrase);
+
+        assertThat(actualResponse.getStatusCode()).isEqualTo(expectedResponse.getStatusCode());
+        assertThat(actualResponse.getReasonPhrase()).isEqualTo(expectedResponse.getReasonPhrase());
+    }
+
+    @DisplayName("지원하지 않는 메서드로 요청할 경우 405 에러를 반환한다")
+    @Test
+    void methodTest(){
+        String requestExample =
+                """
+                DELETE /index.html HTTP/1.1
+                Host: localhost:8080
+                Connection: keep-alive
+                Accept:
+                """;
+
+        HttpResponse actualResponseMsg = sendRequestAndGetResponse(requestExample);
+        HttpResponse expectedResponseMsg = generateExpectedResponse(METHOD_NOT_ALLOWED.getStatusCode(), METHOD_NOT_ALLOWED.getReasonPhrase());
 
         assertThat(actualResponseMsg.getStatusCode()).isEqualTo(expectedResponseMsg.getStatusCode());
         assertThat(actualResponseMsg.getReasonPhrase()).isEqualTo(expectedResponseMsg.getReasonPhrase());
+    }
+
+    private BufferedReader createBufferedReaderFromString(String request) {
+        InputStream is = new ByteArrayInputStream(request.getBytes());
+        return new BufferedReader(new InputStreamReader(is));
+    }
+
+    private HttpResponse generateExpectedResponse(int statusCode, String reasonPhrase) {
+        return new HttpResponse.Builder(statusCode, reasonPhrase).build();
+    }
+
+    private HttpResponse sendRequestAndGetResponse(String requestExample) {
+        BufferedReader br = createBufferedReaderFromString(requestExample);
+        HttpRequest httpRequest = new HttpRequest(br);
+        return frontRouter.route(httpRequest);
     }
 }
