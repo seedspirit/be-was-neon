1573471bea45033dd44aca38d3112285660c3cda
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-21 15:53:05 +0900

    test: 로그인 핸들러 기능 테스트 코드 추가

---
+++ src/test/java/webserver/LoginHandlerTest.java
@@ -0,0 +1,32 @@
+package webserver;
+
+import db.SessionDatabase;
+import model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LoginHandlerTest {
+
+    @DisplayName("세션 ID로 데이터베이스에서 특정 유저 객체를 찾을 수 있다")
+    @Test
+    void findUserBySessionIdTest() throws NoSuchFieldException, IllegalAccessException {
+        Session session = getMockSession();
+        User user = new User("ex", "ex", "ex", "ex@email.com");
+        SessionDatabase.addSession(session, user);
+        assertThat(SessionDatabase.findUserBySessionId("100")).isEqualTo(user);
+    }
+
+    private Session getMockSession() throws NoSuchFieldException, IllegalAccessException {
+        Class<Session> clazz = Session.class;
+        Field sessionId = clazz.getDeclaredField("sessionId");
+        sessionId.setAccessible(true);
+        Session session = new Session();
+        sessionId.set(session, "100");
+        return session;
+    }
+}
