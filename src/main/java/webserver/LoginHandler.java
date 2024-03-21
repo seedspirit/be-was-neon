99b23522806cffa145d02f634a31bed297f9e937
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-21 14:04:14 +0900

    feat: 세션과 쿠키를 사용한 로그인 기능 구현

--- src/main/java/webserver/LoginHandler.java
+++ src/main/java/webserver/LoginHandler.java
@@ -1,24 +1,34 @@
 package webserver;
 
 import db.Database;
+import db.SessionDataBase;
 import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Optional;
+import java.util.*;
 
 import static util.constants.Delimiter.EQUAL_SIGN;
 
 public class LoginHandler {
-    Map<String, String> params;
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private Map<String, String> params;
 
-    public LoginHandler(String body){
+    public LoginHandler(){
         this.params = new HashMap<>();
-        parseBody(body);
     }
+
+    public String apply(String body) throws NoSuchElementException {
+        parseBody(body);
+        User user = findUserByUserId(params.get("username"));
+        String sessionId = generateSessionId();
+        registerSessionId(sessionId, user);
+        logger.debug("로그인 성공! Name: {}, SessionId: {}", user.getName(), sessionId);
+        return sessionId;
+    }
+
     private void parseBody(String body) {
         String[] parameters = body.split("&");
         for (String parameter : parameters) {
@@ -29,11 +39,20 @@ public class LoginHandler {
         }
     }
 
-    private User findUserByUserID(String userID) throws NoSuchElementException {
+    private User findUserByUserId(String userID) throws NoSuchElementException {
         Optional<User> userOptional = Optional.ofNullable(Database.findUserById(userID));
         if (userOptional.isEmpty()){
             throw new NoSuchElementException(": 존재하지 않는 회원입니다");
         }
         return userOptional.get();
+    }
+
+    private String generateSessionId() {
+        long randomLong = (long) (Math.random() * 10000000000L) + 1;
+        return String.valueOf(randomLong);
+    }
+
+    private void registerSessionId(String sessionId, User user){
+        SessionDataBase.addSession(sessionId, user);
     }
 }
