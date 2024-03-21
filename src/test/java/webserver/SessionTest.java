9d3af79cd252d9d1044f514920bd490e94f9e3bb
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-21 15:53:43 +0900

    test: 리플렉션을 이용한 세션 클래스 테스트 코드 추가

---
+++ src/test/java/webserver/SessionTest.java
@@ -0,0 +1,23 @@
+package webserver;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SessionTest {
+
+    @DisplayName("세션id가 100인 Session의 toString을 호출하면 sid=100; Path=/을 반환한다")
+    @Test
+    void sessionToStringTest() throws NoSuchFieldException, IllegalAccessException {
+        Class<Session> clazz = Session.class;
+        Field sessionId = clazz.getDeclaredField("sessionId");
+        sessionId.setAccessible(true);
+        Session session = new Session();
+        sessionId.set(session, "100");
+        String expected = "sid=100; Path=/";
+        assertThat(session.toString()).isEqualTo(expected);
+    }
+}
