1041a7b0b43807a2ce5d2aaa416bda8064bc4f5f
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-25 20:22:28 +0900

    test: 세션 정보 삭제 기능 테스트 코드 추가

--- src/test/java/db/SessionDatabaseTest.java
+++ src/test/java/db/SessionDatabaseTest.java
@@ -1,6 +1,7 @@
 package db;
 
 import model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import webserver.session.Cookie;
@@ -11,17 +12,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class SessionDatabaseTest {
 
+    private Cookie cookie;
+    private User user;
+
+    @BeforeEach
+    void setCookie() throws NoSuchFieldException, IllegalAccessException {
+        SessionDatabase.clearDatabase();
+        this.cookie = getMockCookie();
+        this.user = new User("ex", "ex", "ex", "ex@email.com");
+        SessionDatabase.addRecord(cookie, user);
+    }
+
     @DisplayName("세션 ID로 데이터베이스에서 특정 유저 객체를 찾을 수 있다")
     @Test
-    void findUserBySessionIdTest() throws NoSuchFieldException, IllegalAccessException {
-        Cookie cookie = getMockSession();
-        User user = new User("ex", "ex", "ex", "ex@email.com");
-        SessionDatabase.addRecord(cookie, user);
+    void findUserBySessionIdTest() {
         String sessionId = "100";
         assertThat(SessionDatabase.findUserBySessionId(sessionId)).isEqualTo(user);
     }
 
-    private Cookie getMockSession() throws NoSuchFieldException, IllegalAccessException {
+    @DisplayName("세션 ID로 데이터베이스에서 세션 정보를 삭제할 수 있다")
+    @Test
+    void recordRemovalTest() {
+        String sessionId = "100";
+        SessionDatabase.removeRecordOf(sessionId);
+        assertThat(SessionDatabase.isSessionIdExists(sessionId)).isFalse();
+    }
+
+    private Cookie getMockCookie() throws NoSuchFieldException, IllegalAccessException {
         Class<Cookie> clazz = Cookie.class;
         Field sessionId = clazz.getDeclaredField("sessionId");
         sessionId.setAccessible(true);
