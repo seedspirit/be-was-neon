c5f6f97fd092fc671e7f7b55e10a7d238b14a0dd
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-03-27 18:22:33 +0900

    feat: 인가에 따른 접근성 별로 URL을 나누고 접근성을 확인하는 기능 추가

--- src/main/java/webserver/URLConstants.java
+++ src/main/java/webserver/URLConstants.java
@@ -1,9 +1,29 @@
 package webserver;
 
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class URLConstants {
     public static final String DEFAULT_INDEX_PAGE = "/index.html";
     public static final String LOGIN_USER_DEFAULT_INDEX_PAGE = "/main/index.html";
+    public static final String LOGIN_INDEX_PAGE = "/login/index.html";
     public static final String LOGIN_FAILED_PAGE = "/login/login_failed.html";
     public static final String STATIC_DIR_PATH = "/static";
     public static final String REGISTRATION_FAILED_PAGE = "/registration/register_failed.html";
+    public static final String ARTICLE_DIRECTORY = "/article.*";
+    public static final String COMMENT_DIRECTORY = "/comment.*";
+
+    public enum AccessLevel {
+        PRIVATE(List.of(LOGIN_USER_DEFAULT_INDEX_PAGE, ARTICLE_DIRECTORY, COMMENT_DIRECTORY));
+
+        private final List<String> urls;
+
+        AccessLevel(List<String> urls) {
+            this.urls = urls;
+        }
+
+        public static boolean belongsToPublic(String url){
+            return PRIVATE.urls.stream().noneMatch(u -> Pattern.compile(u).matcher(url).matches());
+        }
+    }
 }
