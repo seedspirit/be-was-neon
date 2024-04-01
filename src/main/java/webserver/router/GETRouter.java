895a5eb47896976ea7126de6627a71f1ba65b5d4
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-04-01 16:19:08 +0900

    feat: 로그인 시 메인페이지에 유저 이름 표기 기능 추가

--- src/main/java/webserver/router/GETRouter.java
+++ src/main/java/webserver/router/GETRouter.java
@@ -1,9 +1,6 @@
 package webserver.router;
 
-import webserver.handler.ExceptionHandler;
-import webserver.handler.Handler;
-import webserver.handler.ResourceLoadHandler;
-import webserver.handler.UserListPageBuildHandler;
+import webserver.handler.*;
 import webserver.httpMessage.httpRequest.HttpRequest;
 import webserver.httpMessage.httpRequest.RequestLine;
 import webserver.httpMessage.httpResponse.HttpResponse;
@@ -12,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
+import static webserver.URLConstants.DEFAULT_INDEX_PAGE;
 import static webserver.httpMessage.HttpConstants.REQUEST_TARGET;
 
 public class GETRouter implements Router {
@@ -22,16 +20,25 @@ public class GETRouter implements Router {
     public GETRouter(){
         this.handlers = new HashMap<>();
         handlers.put("/user/list", new UserListPageBuildHandler());
+        handlers.put("/main/index.html", new CustomUserHtmlBuildHandler());
         this.exceptionHandler = new ExceptionHandler();
     }
 
     public HttpResponse route(HttpRequest httpRequest) {
         try {
             RequestLine requestLine = httpRequest.getRequestLine();
-            Handler handler = handlers.getOrDefault(requestLine.getValueOf(REQUEST_TARGET), new ResourceLoadHandler());
+            String finalUrl = editMainPath(requestLine.getValueOf(REQUEST_TARGET));
+            Handler handler = handlers.getOrDefault(finalUrl, new ResourceLoadHandler());
             return  handler.handleRequest(httpRequest);
         } catch (Exception exception) {
             return exceptionHandler.handleException(exception);
         }
+    }
+
+    private String editMainPath(String path){
+        if(path.equals("/main")){
+            return path + DEFAULT_INDEX_PAGE;
+        }
+        return path;
     }
 }
