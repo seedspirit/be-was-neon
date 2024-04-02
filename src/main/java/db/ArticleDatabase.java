6afd19f06a232fcda0e2d82f45c36b72209167f4
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-04-02 16:00:46 +0900

    feat: Artilce 객체를 저장하는 데이터베이스 클래스 추가

---
+++ src/main/java/db/ArticleDatabase.java
@@ -0,0 +1,37 @@
+package db;
+
+import webserver.Article;
+
+import java.util.*;
+
+public class ArticleDatabase {
+    private static final Map<Long, Article> articleDatabase = new LinkedHashMap<>();
+
+    public static void addRecord(Long seq, Article article){
+        articleDatabase.put(seq, article);
+    }
+
+    public static Article getArticle(Long seq) {
+        return articleDatabase.get(seq);
+    }
+
+    public static void removeRecord(Long seq){
+        articleDatabase.remove(seq);
+    }
+
+    public static String generateArticleListHTML() {
+        final String TABLE_HTML_FORMAT = """
+                <tbody>
+                    %s
+                </tbody>
+                            """;
+        StringBuilder result = new StringBuilder();
+        for (Article article : articleDatabase.values()){
+            result.append("<tr>\n")
+                    .append(String.format("<td>%s</td>\n", article.getTitle()))
+                    .append(String.format("<td>%s</td>\n", article.getWriter()))
+                    .append("</tr>\n");
+        }
+        return String.format(TABLE_HTML_FORMAT, result.toString());
+    }
+}
