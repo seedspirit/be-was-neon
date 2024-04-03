ceaf5114443c1b27edee0c05ccc8cfaa495b2832
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-04-03 14:55:39 +0900

    fix: URL 접근 보안성 수정

--- src/main/java/webserver/URLConstants.java
+++ src/main/java/webserver/URLConstants.java
@@ -6,17 +6,18 @@ import java.util.regex.Pattern;
 public class URLConstants {
     public static final String DEFAULT_INDEX_PAGE = "/index.html";
     public static final String LOGIN_USER_DEFAULT_INDEX_PAGE = "/main/index.html";
+    public static final String MAIN_DIRECTORY = "/main/.*";
     public static final String LOGIN_INDEX_PAGE = "/login/index.html";
     public static final String LOGIN_FAILED_PAGE = "/login/login_failed.html";
     public static final String STATIC_DIR_PATH = "/static";
     public static final String REGISTRATION_FAILED_PAGE = "/registration/register_failed.html";
     public static final String ARTICLE_DIRECTORY = "/article.*";
     public static final String COMMENT_DIRECTORY = "/comment.*";
-    public static final String USER_DIR = "/user/.*";
+    public static final String USER_DIRECTORY = "/user/.*";
     public static final String USER_LIST_PAGE = "/user/list.html";
 
     public enum AccessLevel {
-        PRIVATE(List.of(LOGIN_USER_DEFAULT_INDEX_PAGE, ARTICLE_DIRECTORY, COMMENT_DIRECTORY, USER_DIR));
+        PRIVATE(List.of(MAIN_DIRECTORY, ARTICLE_DIRECTORY, COMMENT_DIRECTORY, USER_DIRECTORY));
 
         private final List<String> urls;
 
