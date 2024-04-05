87e45d7ce6c6d7530dd88c0931d80122862e7704
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2024-04-05 14:54:24 +0900

    feat: 아티클에 저장된 내용을 html로 출력하는 기능 추가

--- src/main/java/webserver/Article.java
+++ src/main/java/webserver/Article.java
@@ -41,4 +41,48 @@ public class Article {
     public String getWriter() {
         return writer;
     }
+
+    public String generatePostHTML(){
+        String WRITER_MARKER = "articleWriter";
+        String CONTENT_MARKER = "articleContent";
+        String IMAGE_MARKER = "articleImage";
+        String html = HTMLFormat.replaceFirst(WRITER_MARKER, writer)
+                .replaceFirst(CONTENT_MARKER, contentString)
+                .replaceFirst(IMAGE_MARKER, generateImageTage());
+        return html;
+    }
+
+    private String generateImageTage(){
+        return "src=\"data:" + imageFile.getMimeType() + ";base64," + imageFile.toBase64() + "\"";
+    }
+
+    private final String HTMLFormat = """
+            <div class="post">
+            <div class="post__account">
+                        <img class="post__account__img" />
+                        <p class="post__account__nickname">articleWriter</p>
+                      </div>
+                      <img class="post__img" articleImage />
+                      <div class="post__menu">
+                        <ul class="post__menu__personal">
+                          <li>
+                            <button class="post__menu__btn">
+                              <img src="../img/like.svg" />
+                            </button>
+                          </li>
+                          <li>
+                            <button class="post__menu__btn">
+                              <img src="../img/sendLink.svg" />
+                            </button>
+                          </li>
+                        </ul>
+                        <button class="post__menu__btn">
+                          <img src="../img/bookMark.svg" />
+                        </button>
+                      </div>
+                      <p class="post__article">
+                        articleContent
+                      </p>
+                      </div>
+            """;
 }
